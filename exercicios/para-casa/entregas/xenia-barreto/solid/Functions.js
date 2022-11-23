3810bf0d6c6008587f0818a5cc05c81f64b0ec1b
xeniabarreto <xeniabarreto22@gmail.com>
2022-11-22 22:13:24 -0300

    Updates

---
+++ exercicios/para-casa/entregas/xenia-barreto/solid/Functions.js
@@ -0,0 +1,10 @@
+class MenuDrinks {
+    createMenuDrinks(){
+
+    }
+
+    offerMenu(){
+        
+    }
+}
+
--- exercicios/para-casa/entregas/xenia-barreto/solid/Main.js
+++ exercicios/para-casa/entregas/xenia-barreto/solid/Main.js
@@ -1,3 +1,4 @@
 const Juice = require('./Juice')
 const Soda = require('./Soda')
 const Tea = require('./Tea')
+
