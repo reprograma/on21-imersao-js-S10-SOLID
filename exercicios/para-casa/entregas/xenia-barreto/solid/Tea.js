564f10269d523fff8250581d6c38e1601b62dff9
xeniabarreto <xeniabarreto22@gmail.com>
2022-11-24 20:06:31 -0300

    Fix tea class

--- exercicios/para-casa/entregas/xenia-barreto/solid/Tea.js
+++ exercicios/para-casa/entregas/xenia-barreto/solid/Tea.js
@@ -1,12 +1,19 @@
-class Tea {
-    
-    constructor(id, name, price, taste, brand) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.taste = taste;
-        this.brand = brand;
-    }
+const Drink = require("./Drink");
+
+class Tea extends Drink {
+  constructor(id, name, price, taste, brand) {
+    super(id, name, price, taste, brand);
+
+    this.constructor.teaList.push({
+      id: this.id,
+      name: this.name,
+      price: this.price,
+      taste: this.taste,
+      brand: this.brand,
+    });
+  }
+
+  static teaList = [];
 }
 
-module.exports = Tea
+module.exports = Tea;
