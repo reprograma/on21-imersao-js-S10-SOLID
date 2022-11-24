15194a61ee698b50df4fdf249acad9cd457b046b
xeniabarreto <xeniabarreto22@gmail.com>
2022-11-24 19:55:26 -0300

    Fix juice class

--- exercicios/para-casa/entregas/xenia-barreto/solid/Juice.js
+++ exercicios/para-casa/entregas/xenia-barreto/solid/Juice.js
@@ -1,12 +1,19 @@
-class Juice {
+const Drink = require("./Drink");
+class Juice extends Drink {
     
     constructor(id, name, price, taste, brand) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.taste = taste;
-        this.brand = brand;
+        super(id, name, price, taste, brand)
+        
+        this.constructor.juiceList.push({
+        id: this.id,
+        name: this.name,
+        price: this.price,
+        taste: this.taste,
+        brand: this.brand
+        });
     }
+
+    static juiceList = []
 }
 
 module.exports = Juice
