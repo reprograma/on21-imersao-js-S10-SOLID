7b35191332071dc6a788e5c3ad3d64c558559d88
Mariana Trombini <mariana.trombini@dasa.com.br>
2022-11-24 12:05:45 -0300

    :sparkles: feat: add Tea class

---
+++ exercicios/para-casa/entregas/mariana-trombini/Tea.js
@@ -0,0 +1,18 @@
+const { Drink } = require('./Drink');
+
+class Tea extends Drink {
+  constructor(id, name, price, brand, flavor) {
+    super(id, name, price, brand, flavor)
+    this.constructor.teas.push({
+      id: this.id,
+      name: this.name,
+      price: this.price,
+      brand: this.brand,
+      flavor: this.flavor
+    })
+  }
+  static teas = []
+}
+
+module.exports = { Tea }
+
