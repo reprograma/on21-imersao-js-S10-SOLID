3d0731b6cf381e99921023e4df1b6a666e025707
Silvovisk <vascsilvia@gmail.com>
2022-11-24 23:11:12 -0300

    exercicio para casa

--- exercicios/para-casa/exLar.js
+++ exercicios/para-casa/exLar.js
@@ -2,3 +2,32 @@
 Refatore o exercicio da Semana 9 e aplique SOLID (Aplique Classe se necessário)
 
 */
+
+class Car {
+    construtor(nome, modelo, ano) {
+      this.name = nome;
+      this.model = model;
+      this.year = year;
+    }
+    getCar(id) {
+      return this.http.get("api/cars/" + id);
+    }
+    saveCar() {
+      return this.post("api/cars", {
+        name: this.name,
+        year: this.year,
+        model: this.model,
+      });
+    }
+  }
+  
+  //O
+  let modelo = prompt("Informe o modelo do veiculo");
+  if (modelo == "corolla") {
+    console.log("Especificações do Corolla");
+  } else if (modelo == "Uninho") {
+    console.log("Especificações Uninho");
+  }
+
+  
+  
