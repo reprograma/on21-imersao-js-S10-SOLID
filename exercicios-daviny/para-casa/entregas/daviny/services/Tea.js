66f469b31ad97e09c99b17cece120f49ac6c4da3
Daviny Vidal <daviny@vidal.dev.br>
2022-11-27 20:33:56 -0300

    refatorando

---
+++ exercicios-daviny/para-casa/entregas/daviny/services/Tea.js
@@ -0,0 +1,19 @@
+const Drink = require("./Drink");
+
+class Tea extends Drink {
+  constructor(id, name, price, taste, brand) {
+    super(id, name, price, taste, brand);
+
+    this.constructor.teaList.push({
+      id: this.id,
+      name: this.name,
+      price: this.price,
+      taste: this.taste,
+      brand: this.brand,
+    });
+  }
+
+  static teaList = [];
+}
+
+module.exports = Tea;
